49c0117b8d6756514bf6f8a570f8ff221f9489f4
jihye <lovinsurfaces@gmail.com>
2026-04-12 15:57:42 +0900

    오류 수정

--- server/server.js
+++ server/server.js
@@ -10,7 +10,9 @@ app.use(express.json());
 app.use(
   cors({
     origin:
-      process.env.NODE_ENV === "production" ? "" : "http://localhost:5173",
+      process.env.NODE_ENV === "production"
+        ? "https://askstars.vercel.app/"
+        : "http://localhost:5173",
   }),
 );
 
